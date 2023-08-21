54afba946c200875027c8c4d1839af5652db53a8
sahan <hyanjusahan@gmail.com>
2023-08-21 12:50:49 +0545

    dark js

--- dogcare/assets/js/custom.js
+++ dogcare/assets/js/custom.js
@@ -7,3 +7,19 @@ lightGallery(document.getElementById('lightgallery'), {
     speed: 500,
     // ... other settings
 });
+const darkMode = document.querySelector(".color-change");
+console.log(darkMode);
+const body = document.querySelector("body");
+const moon = darkMode.children;
+console.log(moon[0]);
+darkMode.addEventListener("click", function () {
+  if (moon[0].className == "fa-moon") {
+    body.classList.remove("dark");
+    moon[0].classList.remove("fa-moon");
+    moon[0].classList.add("fa-sun");
+  } else {
+    body.classList.add("dark");
+    moon[0].classList.add("fa-moon");
+    moon[0].classList.remove("fa-sun");
+  }
+});
